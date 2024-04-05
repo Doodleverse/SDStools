53b280cf009ce242130b5623e7143fb0fb79d751
mlundine <lundinemark@gmail.com>
2024-04-05 14:19:04 -0700

    update image_classification

--- fromMark/image_classification/coastal_image_classification.py
+++ fromMark/image_classification/coastal_image_classification.py
@@ -15,7 +15,7 @@ def load_dataset(training_data_directory,
                  image_size,
                  batch_size):
     """
-    loads in training data
+    loads in training data in training and validation sets
     inputs:
     training_data_directory (str): path to the training data
     returns:
@@ -42,6 +42,12 @@ def data_augmentation(images):
     return images
 
 def define_model(input_shape, num_classes=2):
+    """
+    Defines the classification model
+    inputs:
+    input_shape (tuple (xdim, ydim)): shape of images for model
+    num_classes (int, optional): number of classes for the model
+    """
     inputs = keras.Input(shape=input_shape)
 
     # Entry block
@@ -92,6 +98,16 @@ def train_model(model,
                 val_ds,
                 model_folder,
                 epochs=100):
+    """
+    Trains the good/bad classification model
+    inputs:
+    model (keras model): this is defined in define_model()
+    image_size (tuple, (xdim, ydim): dimensions of images for model
+    train_ds: training dataset obtained from load_dataset
+    val_ds: validation dataset obtained from load_dataset
+    model_folder (str): path to save the model to
+    epochs (int, optional): number of epochs to train for
+    """
     model = make_model(input_shape=image_size + (3,), num_classes=2)
     keras.utils.plot_model(model, show_shapes=True)
     ckpt_file = os.path.join(model_folder, "model_{epoch}.keras")
@@ -114,6 +130,12 @@ def train_model(model,
 def sort_images(inference_df_path,
                 inference_images_path,
                 ):
+    """
+    Uses model results to sort images into good and bad folders
+    inputs:
+    inference_df_path (str): path to the model result csv
+    inference_images_path (str): path to the directory containing images model was run on
+    """
     bad_dir = os.path.join(inference_images_path, 'bad')
     good_dir = os.path.join(inference_images_path, 'good')
     dirs = [bad_dir, good_dir]
@@ -137,7 +159,19 @@ def run_inference(path_to_model_ckpt,
                   output_folder,
                   image_size,
                   result_path):
-    
+    """
+    Runs the trained model on images, classifying them either as good or bad
+    Saves the results to a csv (image_path, class (good or bad), score (0 to 1)
+    Sorts the images into good or bad folders
+    inputs:
+    path_to_model_ckpt (str): path to the saved keras model
+    path_to_inference_imgs (str): path to the folder containing images to run the model on
+    output_folder (str): path to save outputs to
+    image_size (tuple, (xdim, ydim)): size of images for model
+    result_path (str): csv path to save results to
+    returns:
+    result_path (str): csv path of saved results
+    """
     model = keras.saving.load_model(path_to_model_ckpt)
     im_paths = glob.glob(path_to_inference_imgs+'\*.jpg')
     im_classes = [None]*len(im_paths)
@@ -169,6 +203,12 @@ def run_inference(path_to_model_ckpt,
 
 def sort_images(inference_df_path,
                 inference_images_path):
+    """
+    Using model results to sort the images the model was run on into good and bad folders
+    inputs:
+    inference_df_path (str): path to the csv containing model results
+    inference_images_path (str): path to the directory containing the inference images
+    """
     bad_dir = os.path.join(inference_images_path, 'bad')
     good_dir = os.path.join(inference_images_path, 'good')
     dirs = [bad_dir, good_dir]
@@ -193,6 +233,7 @@ def plot_history(history,
     This makes a plot of the loss curve
     inputs:
     history: history object from model.fit_generator
+    history_save_path (str): path to save the plot to
     """
     plt.plot(history.history['loss'], color='b')
     plt.plot(history.history['val_loss'], color='r')
@@ -201,20 +242,37 @@ def plot_history(history,
     plt.xlabel('Epoch')
     plt.legend(['Training Data', 'Validation Data'],loc='upper right')
     plt.savefig(history_save_path, dpi=300)
+    plt.close('all')
     
 def training(path_to_training_data,
-             epochs,
-             output_folder):
+             output_folder,
+             epochs=100):
+    """
+    Training the good/bad classification model
+    inputs:
+    path_to_training_data (str): path to the directory containing good and bad subdirectories
+    output_folder (str): path to save outputs to
+    epoch (int, optional): number of epochs to train for
+    """
+    ##Making new directories
+    try:
+        os.mkdir(output_folder)
+    except:
+        pass
     model_folder = os.path.join(output_folder, 'models')
     history_save_path = os.path.join(model_folder, 'history.png')
     try:
         os.mkdir(model_folder)
     except:
         pass
+
+    ##Setting image size for model and loading datasets
     image_size = (256, 256)
     train_ds, val_ds = load_dataset(path_to_training_data,
                                     image_shape,
                                     32)
+
+    ##Defining and then training model
     model = define_model(input_shape=image_size + (3,), num_classes=2)
     model, history, ckpt_file = train_model(model,
                                             image_size,
@@ -222,6 +280,8 @@ def training(path_to_training_data,
                                             val_ds,
                                             model_folder,
                                             epochs=epochs)
+
+    ##Plotting and saving loss curve
     plot_history(history, history_save_path)
     return ckpt_file
     
